f852eea772053bbc07998e2c664bfded162638d8
Christopher Moser <moser121@icloud.com>
2018-02-22 23:33:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+var choclateBars = ['snickers', 'hundred grand', 'kitkat','skittles']
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element){
+  array.unshift(element)
+  return array
+}
+function addElementToEndOfArray(array,element){
+  return[...array, element]
+}
+function destructivelyRemoveElmentFromBeginningOfArray(array,element){
+  array.push(element)
+}
+function accessElementInArray(array,element){
+  re
+}
+
+    
+  
